cb43f49243e3695de088ef5ac86fec5df0fc7e6b
qinuen <qinuen@qq.com>
2018-08-23 11:59:26 +0800

    学习仿做香哈菜谱
    学习仿做香哈菜谱

---
+++ 菜谱/pages/index/index.js
@@ -0,0 +1,19 @@
+Page({
+  data:{
+    indicatorDots:true,
+    indicatorColor:"#0f0",
+    interval:2000,
+
+    food:[{
+      "imgUrl":"../../images/uploads/banner1.jpg"
+    },
+      {
+        "imgUrl": "../../images/uploads/banner2.jpg"
+      },
+      {
+        "imgUrl": "../../images/uploads/banner3.jpg"
+      }
+    ]
+
+  }
+})
