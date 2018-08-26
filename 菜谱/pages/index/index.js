fa61b786914283ce2de7a06b5ccf9aad3c911dcc
qinuen <qinuen@qq.com>
2018-08-26 22:49:03 +0800

    8/26
    增加了热门推荐

--- 菜谱/pages/index/index.js
+++ 菜谱/pages/index/index.js
@@ -1,17 +1,35 @@
 Page({
-  data:{
-    indicatorDots:true,
-    indicatorColor:"#0f0",
-    interval:2000,
+  data: {
+    indicatorDots: true,
+    indicatorColor: "#0f0",
+    interval: 2000,
 
-    food:[{
-      "imgUrl":"../../images/uploads/banner1.jpg"
+    food: [{
+      "imgUrl": "../../images/uploads/banner1.jpg"
+    },
+    {
+      "imgUrl": "../../images/uploads/banner2.jpg"
+    },
+    {
+      "imgUrl": "../../images/uploads/banner3.jpg"
+    }
+    ],
+
+    hot:[{
+      "imgUrl": "../../images/uploads/rec1.jpg",
+      "text":"素菜"
+    },
+    {
+        "imgUrl": "../../images/uploads/rec2.jpg",
+        "text": "荤菜"
     },
       {
-        "imgUrl": "../../images/uploads/banner2.jpg"
+        "imgUrl": "../../images/uploads/rec3.jpg",
+        "text": "水煮"
       },
       {
-        "imgUrl": "../../images/uploads/banner3.jpg"
+        "imgUrl": "../../images/uploads/rec4.jpg",
+        "text": "油炸"
       }
     ]
 
